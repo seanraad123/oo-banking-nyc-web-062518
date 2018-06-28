0d1f2c6fc73c026a9df864161fcf3c6021046ca6
seanraad123 <seanconrad123@gmail.com>
2018-06-28 18:18:20 -0400

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,37 @@
+require 'pry'
+
 class BankAccount
 
+	attr_reader :name
+	attr_accessor :balance, :status
+
+	def initialize(name)
+		@name = name
+		@balance = 1000
+		@status = "open"
+	end
+
+	def deposit(amount)
+		self.balance += amount
+	end
+
+	def display_balance
+		"Your balance is $#{self.balance}."
+	end
+
+	def valid?
+		if self.status == 'open' && self.balance > 0
+			return true
+		else
+			return false
+		end
+	end
+
+	def close_account
+		self.status = "closed"
+	end
 end
+
+bank1 = BankAccount.new("Lillian's Bank")
+
+# binding.pry
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,67 @@
+require 'pry'
+
 class Transfer
   # your code here
-end
+  attr_accessor :sender, :receiver, :status, :amount, :count
+
+
+  @@transactions = []
+  
+  def initialize(sender, receiver, amount)
+  	@sender = sender
+  	@receiver = receiver
+  	@status = "pending"
+  	@amount = amount
+  	@count = 0
+
+  end
+
+  def valid? 
+  	if sender.valid? && receiver.valid?
+  		true 
+  	else 
+  		false
+  	end
+  end 
+    
+  def execute_transaction
+  	if self.count == 0 && sender.valid? == true && sender.balance > self.amount
+  		 # binding.pry
+	  	receiver.deposit(self.amount) 
+	  	sender.deposit(-self.amount)
+	  	self.status = "complete"
+  		self.count += 1
+  		@@transactions << self.object_id
+  	else
+  		self.status = "rejected"
+	  	return "Transaction rejected. Please check your account balance."
+
+  	end
+  end 
+
+  def self.get_all_transactions
+  	@@transactions
+  end
+
+  def reverse_transfer
+  	@@transactions.each do |x|
+  		if self.object_id == x
+  			receiver.deposit(-self.amount) 
+			sender.deposit(self.amount)
+			self.status = "reversed"
+		  end
+		end
+	end
+
+
+arraay.includes(sean)
+ 
+ end
+
+sean = BankAccount.new ("Sean's account")
+lily = BankAccount.new ("Lil's account")
+transfer = Transfer.new(sean, lily, 40)
+transfer2 = Transfer.new(lily, sean, 75)
+
+transfer.execute_transaction
+transfer2.execute_transaction
